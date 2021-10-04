ebba5c8696c55d85345bf282acc1c31f32dffd12
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-04 23:10:46 +0800

    Update buildDocker.groovy

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,3 +1,13 @@
-def call(){
+def buildNum(){
   echo "Build image with tag: ${env.BUILD_ID}"
+}
+
+def myAppDocker(){
+  myapp = docker.build("geraldine28/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
+}
+
+def pushDocker(){
+  docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+    myapp.push("latest")
+    myapp.push("${env.BUILD_ID}")
 }
