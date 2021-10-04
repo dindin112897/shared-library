83f32bdbb46b72a12d245b35dc2f8c141c301ca8
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-04 23:46:14 +0800

    Update buildDocker.groovy

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -6,6 +6,10 @@ def myAppDocker(){
   myapp = docker.build("geraldine28/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
 }
 
+def myAppNpm(){
+  myapp = docker.build("geraldine28/nodejs-test:${env.BUILD_ID}")
+}
+
 def pushDocker(){
   docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
     myapp.push("latest")
