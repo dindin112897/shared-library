42f57ac89244d10b9ef2a24241af8e710b7706f2
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-12 18:19:36 +0800

    Update deletetxt.groovy

--- vars/deletetxt.groovy
+++ vars/deletetxt.groovy
@@ -1,1 +1,9 @@
-delete file.
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-geraldine"]) {
+    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+        awsIdentity()
+        s3Delete( 
+            bucket: stageParams.s3Bucket,
+            path: "delete.txt"
+        )
+    }
+}
