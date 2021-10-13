472ba8e5519c96e016b54096fb0aae2ffbe05bc5
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-13 11:52:34 +0800

    Update uploadHellotxt.groovy

--- vars/uploadHellotxt.groovy
+++ vars/uploadHellotxt.groovy
@@ -1,9 +1,10 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-geraldine"]) {
-    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+def call(Map stageParams = [:]) {
+    withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
-            bucket: stageParams.s3Bucket,
-            file: "helloworld.txt"
+            bucket: "${stageParams.s3Bucket}",
+            file: "${stageParams.file}"
         )
     }
 }
+// awsRegion: "us-east-1", s3Bucket: "testbucket-geraldine" , file: "helloworld.txt"
