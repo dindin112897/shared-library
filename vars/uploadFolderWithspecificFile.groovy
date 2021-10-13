d4f11e21ea03affac984ec2ad320bb399426da10
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-13 16:06:10 +0800

    Create uploadFolderWithspecificFile.groovy

---
+++ vars/uploadFolderWithspecificFile.groovy
@@ -0,0 +1,9 @@
+def call(Map stageParams = [:]) {
+    withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {
+        awsIdentity()
+        s3Upload( 
+            bucket: "${stageParams.s3Bucket}",
+            includePathPattern: "${stageParams.file}"
+        )
+    }
+}
