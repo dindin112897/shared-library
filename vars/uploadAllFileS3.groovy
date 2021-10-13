e52652e874632c82e133a623807cf2e63bd2411b
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-13 16:48:48 +0800

    Update and rename uploadAllFileS3.groovy to uploadAllS3.groovy

--- vars/uploadAllFileS3.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-geraldine"]) {
-    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
-        awsIdentity()
-        s3Upload( 
-            bucket: stageParams.s3Bucket,
-            includePathPattern: "**/*"
-        )
-    }
-}
