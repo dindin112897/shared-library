18bac0c0ee355db74ec4603520e08534c0006933
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-13 11:29:19 +0800

    Update cf_deploy_ec2.groovy

--- vars/cf_deploy_ec2.groovy
+++ vars/cf_deploy_ec2.groovy
@@ -3,6 +3,7 @@ def call() {
         agent any
         environment {
             AWS_CRED = 'cloud_user'
+            AWS_REGION = 'us-east-1'
         }
         stages {
             stage('Upload template to S3') {                  
