1c5a873c1b6cf6213d20808e24fcc927bd6fccca
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-13 16:29:03 +0800

    Update uploadTemplateToS3.groovy

--- vars/uploadTemplateToS3.groovy
+++ vars/uploadTemplateToS3.groovy
@@ -2,7 +2,8 @@ def call(Map stageParams = [:]) {
     withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
-            includePathPattern: "*.yml", "sample/sampleThree.txt",
+            includePathPattern: "*.yml",
+            includePathPattern: "sample/sampleThree.txt",
             bucket: "${stageParams.s3Bucket}"
         )
     }
