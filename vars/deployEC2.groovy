1038b0fc77884353d72c8d619dc1d05541833cd4
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-13 14:57:07 +0800

    Update deployEC2.groovy

--- vars/deployEC2.groovy
+++ vars/deployEC2.groovy
@@ -1,14 +1,15 @@
-def call(Map stageParams = [awsRegion: "us-east-1"]){
-  withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+def call(Map stageParams = [:]){
+  withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {
         awsIdentity()
         cfnCreateChangeSet(
-          stack:'EC2Jenkins-geraldine', 
+          stack: "${stageParams.stack}", 
           changeSet:'my-change-set', 
-          url:'https://testbucket-geraldine.s3.amazonaws.com/deployEC2.yml'
+          url: "${stageParams.url}"
         )
         cfnExecuteChangeSet(
-          stack:'EC2Jenkins-geraldine', 
+          stack:"${stageParams.stack}", 
           changeSet:'my-change-set'
         )
   }
 }
+//awsRegion: "us-east-1" , 'EC2Jenkins-geraldine', 'https://testbucket-geraldine.s3.amazonaws.com/deployEC2.yml'
