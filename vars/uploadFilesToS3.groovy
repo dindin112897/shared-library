90e8c96aa611df5814510eafd984c03f0818cafe
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-14 15:48:10 +0800

    Create uploadFilesToS3.groovy

---
+++ vars/uploadFilesToS3.groovy
@@ -0,0 +1,11 @@
+def call(Map stageParams = [Region: "us-east-1",  bucketName: "test-bucket-2221", s3Path: ""]) {
+    withAWS(region: stageParams.Region, credentials:'awscredentials') {
+        awsIdentity()
+        s3Upload( 
+            workingDir: stageParams.workingDir,
+            includePathPattern: stageParams.stackFileName,
+            bucket: stageParams.bucketName,
+            path: stageParams.s3Path
+        )
+    }
+}
