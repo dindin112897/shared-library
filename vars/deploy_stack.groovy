b8c4b59b157dca6ea8f2cc84ea829ea73c24a04a
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-14 15:46:48 +0800

    Create deploy_stack.groovy

---
+++ vars/deploy_stack.groovy
@@ -0,0 +1,21 @@
+def call(Map stageParams = [awsRegion: "us-east-1"]){
+  withAWS(region: stageParams.awsRegion, credentials:"awscredentials") {
+        awsIdentity()
+        cfnCreateChangeSet(
+          stack: stageParams.stackName, 
+          changeSet:'my-change-set', 
+          tags:['Environment='+stageParams.env],
+		  params:['VpcId='+stageParams.VpcId, 'PublicSubnet1='+stageParams.PublicSubnet1, 'PublicSubnet2='+stageParams.PublicSubnet2],
+          url:'https://'+ stageParams.bucketName + '.s3.amazonaws.com/'+ stageParams.stackFileName
+        )
+		cfnExecuteChangeSet(
+          stack: stageParams.stackName, 
+          changeSet:'my-change-set',
+	  pollInterval: 1000,
+	  timeoutInMinutes: 60
+        )
+		outputs = cfnDescribe(stack:stageParams.stackName)
+		outputs.each { println it }
+
+  }
+}
