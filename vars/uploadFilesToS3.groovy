f20a6c12ab950c51dd24c2ef6d9716626b2c6d07
dindin112897 <87467617+dindin112897@users.noreply.github.com>
2021-10-19 13:24:05 +0800

    Update uploadFilesToS3.groovy

--- vars/uploadFilesToS3.groovy
+++ vars/uploadFilesToS3.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams = [Region: "us-east-1",  bucketName: "test-bucket-2221", s3Path: ""]) {
-    withAWS(region: stageParams.Region, credentials:'awscredentials') {
+    withAWS(region: stageParams.Region, credentials:'cloud_user') {
         awsIdentity()
         s3Upload( 
             workingDir: stageParams.workingDir,
